61f71679909fa8d7c083ffbb028cadd29dba0cf9
Sean Clemmer <sclemmer@bluejeans.com>
2014-06-14 14:44:43 -0700

    Generated cookbook

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name             'bjn_logstash'
+version          "#{raise}.#{raise}.#{raise}"
+description      raise
+long_description raise
+maintainer       "#{raise} / Blue Jeans Network"
+maintainer_email "#{raise}@bluejeans.com"
+
+# Depends
+
+# Attributes
+
+# Recipes
---
+++ recipes/default.rb
@@ -0,0 +1,1 @@
+# bjn_logstash
