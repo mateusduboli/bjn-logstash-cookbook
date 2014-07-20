a7f20a5ae19795c348fca7d806b875bc845a77af
Sean Clemmer <sclemmer@bluejeans.com>
2014-07-20 14:16:36 -0700

    Document attribs

--- Readme.md
+++ Readme.md
@@ -1,1 +1,27 @@
 # bjn_logstash
+
+TODO
+
+
+
+## Requirements
+
+This cookbook depends on `ark` to deploy releases and `bjn_essentials` for
+cookbook helpers. It supports these platforms:
+
+- Ubuntu 12.04
+- Ubuntu 14.04
+
+
+
+## Attributes
+
+See the attribute file for inline documentation.
+
+
+
+## Recipes
+
+### default
+
+This recipe creates the user and installs the code from a tarball.
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,35 @@
-default['logstash']['version']   = '1.4.2'
-default['logstash']['checksum']  = 'd5be171af8d4ca966a0c731fc34f5deeee9d7631319e3660d1df99e43c5f8069'
-default['logstash']['url']       = "https://download.elasticsearch.org/logstash/logstash/logstash-%{version}.tar.gz"
-default['logstash']['debug?']    = false
-default['logstash']['user']      = 'logstash'
-default['logstash']['home']      = '/opt/logstash'
-default['logstash']['logs']      = '/var/log/logstash'
-default['logstash']['conf']      = '/etc/logstash/logstash.conf'
-default['logstash']['agents']    = 1
-default['logstash']['workers']   = 1
+# User for Logstash directories, files, and services
+default['logstash']['user'] = 'logstash'
+
+# Directory to install the Logstash source
+default['logstash']['home'] = '/opt/logstash'
+
+# Directory to store the Logstash logs
+default['logstash']['logs'] = '/var/log/logstash'
+
+# Path to the Logstash configuration
+default['logstash']['conf'] = '/etc/logstash/logstash.conf'
+
+# Version of Logstash to install
+default['logstash']['version'] = '1.4.2'
+
+# SHA-256 checksum for the bundled release
+default['logstash']['checksum'] = \
+  'd5be171af8d4ca966a0c731fc34f5deeee9d7631319e3660d1df99e43c5f8069'
+
+# Full URL (%-pattern-sensitive) for Logstash releases
+default['logstash']['url'] = \
+  'https://download.elasticsearch.org/logstash/logstash/logstash-%{version}.tar.gz'
+
+# Enable debug logging (must be boolean)
+default['logstash']['debug?'] = false
+
+# Number of concurrent Logstash agents
+default['logstash']['agents'] = 1
+
+# Number of concurrent Logstash filter workers
+# For production nodes, consider setting this to node['cpu']['total']
+default['logstash']['workers'] = 1
+
+# Configure a default JVM heap size value
 default['logstash']['heap_size'] = nil
